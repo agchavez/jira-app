50cab757ed6a0d53cc6a0655031e6c881fc421a9
agchavez <angel.chavez.325.ac@gmail.com>
2023-01-14 12:36:57 -0600

    feat: :sparkles: Card de opciones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { Typography } from '@mui/material'
+import { Typography, Grid, Card, CardHeader } from '@mui/material';
 import { Inter } from '@next/font/google'
 import { Layout } from '../components/layouts'
 import { UIProvider } from '../context/ui'
@@ -9,14 +9,29 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <UIProvider>
-      <Layout>
-        <Typography variant="h3" component="h1" sx={{ fontFamily: inter }}>
-          Welcome to JiraApp
-        </Typography>
-      </Layout>
-    </UIProvider>
-      
+      <UIProvider>
+        <Layout>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ p: 2, m: 2, height: 'calc(100vh - 120px)' }}>
+                <CardHeader title="Welcome to Next.js + TypeScript + Material-UI" />
+              </Card>
+              
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ p: 2, m: 2, height: 'calc(100vh - 120px)' }}>
+                <CardHeader title="Welcome to Next.js + TypeScript + Material-UI" />
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ p: 2, m: 2, height: 'calc(100vh - 120px)' }}>
+                <CardHeader title="Welcome to Next.js + TypeScript + Material-UI" />
+              </Card>
+            </Grid>
+          </Grid>
+        </Layout>
+      </UIProvider>
+
     </>
   )
 }
